97ea91906065b12320c7d24bfc3a7fdd276961bf
Kaushik Bhat <128929833+kaushik-bhat@users.noreply.github.com>
2025-08-26 02:14:05 +0530

    Create bad_code.py

---
+++ bad_code.py
@@ -0,0 +1,8 @@
+# hindsight-test-repo/bad_code.py
+import sys  # This is an unused import
+import os   # This is also an unused import
+
+def a_badly_formatted_function(  arg1,arg2   ): # This line has terrible spacing
+    print("This function is poorly formatted")
+    # This function is missing a docstring
+    pass
