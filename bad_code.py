e4af5ebc95ffbfd7671f2fbfd4aa866622e7f5b3
Kaushik Bhat <128929833+kaushik-bhat@users.noreply.github.com>
2025-08-27 00:32:22 +0530

    Update bad_code.py

--- bad_code.py
+++ bad_code.py
@@ -3,7 +3,7 @@ import sys  # This is an unused import
 import os   # This is also an unused import
 
 def a_badly_formatted_function(  arg1,arg2   ): # This line has terrible spacing
-    print("This function is poorly formatted")
+    print("This function is poorly formatted ok")
     # This function is missing a docstring
     pass
 #hii
